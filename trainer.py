e6f1e4e64fadaed793c4ede94f7a69eb9759dc9d
Gohar Shoukat <gohar.i.shoukat@gmail.com>
2022-05-04 09:38:08 +0100

    Working model without validation and upscaling.

--- trainer.py
+++ trainer.py
@@ -13,7 +13,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-
+import os
 import pandas as pd
 import numpy as np
 from sklearn.utils import shuffle
@@ -51,18 +51,55 @@ input4 = keras.Input(shape=(1, ), name = 'bathymetry')
 
 #merge the 4 inputs into one vector for matrix multiplication
 #2 hidden layers and one output layer
-merge = layers.Concatenate()([input1, input2, input3, input4])
-l1 = layers.Dense(15, activation = 'relu', use_bias=True)(merge)
-l2 = layers.Dense(10, activation = 'relu', use_bias=True)(l1)
+merge = layers.Concatenate(axis=1)([input1, input2, input3, input4])
+l1 = layers.Dense(64, activation = 'relu')(merge)
+l2 = layers.Dense(64, activation = 'relu')(l1)
+l3 = layers.Dense(32, activation = 'relu')(l2)
+l4 = layers.Dense(16, activation='relu')(l3)
+l5 = layers.Dense(5, activation='relu')(l4)
 
+l2 = layers.Dropout(0.5)(l2)
 
 #create the 2 outputs for the last layer
-output1 = layers.Dense(1, activation='linear', use_bias=True)(l2) #qc
-output2 = layers.Dense(1, activation='linear', use_bias=True)(l2) #fs
+output1 = layers.Dense(1, activation='linear', name = 'qc')(l5) #qc
+output2 = layers.Dense(1, activation='linear', name = 'fs')(l5) #fs
 
 model = keras.Model(
     inputs = [input1, input2, input3, input4],
     outputs = [output1, output2]
 )
-print('Model built')
-keras.utils.plot_model(model, "multi_input_and_output_model.png", show_shapes=True)
+
+#keras.utils.plot_model(model, "multi_input_and_output_model.png", show_shapes=True)
+model.compile(
+    optimizer = 'adam',
+    loss = {
+        'qc' : keras.losses.MeanSquaredError(),
+        'fs' : keras.losses.MeanSquaredError(),
+    },
+    metrics = ['mse']
+)
+model.summary()
+
+#create directory to save checkpoints
+checkpoint_path = "training_1/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+#save weights at the end of each epoch
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1,
+                                                 )
+
+model.fit(
+    {
+    'depth' : X1, 'lat' : X2, 'lng' : X3,
+    'bathymetry' : X4
+    },
+
+    {
+    'qc' : Y1,
+    'fs' : Y2
+    },
+    batch_size=64, epochs = 100, verbose=2, shuffle=True,callbacks=cp_callback
+
+)
